17199fd02772e1fa1616f0ac18edba8231ef16c8
jiarguello <jhonatan.arguello@gmail.com>
2021-05-18 11:11:43 -0300

    Updates

---
+++ precache-manifest.71c5bed7dccf1292b3ac756a49f9ab3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "532d453eb42ba3bf364ce1c8d6898271",
+    "url": "/react-online-store/index.html"
+  },
+  {
+    "revision": "907b0795bed1b32a242e",
+    "url": "/react-online-store/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b1bd01e5e3d142a29e32",
+    "url": "/react-online-store/static/js/2.e1e9dd2f.chunk.js"
+  },
+  {
+    "revision": "28d36cae8db7790f6fc02920470bed26",
+    "url": "/react-online-store/static/js/2.e1e9dd2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "907b0795bed1b32a242e",
+    "url": "/react-online-store/static/js/main.0ad79416.chunk.js"
+  },
+  {
+    "revision": "d13ae80509a71a60e195",
+    "url": "/react-online-store/static/js/runtime-main.f725dc44.js"
+  }
+]);
