c798accd76bcab7d43da66846419326284a335a6
jhon <jhonatan.arguello@gmail.com>
2021-06-13 14:16:27 -0300

    Deploy.

---
+++ build/precache-manifest.3d4cc44addb94eebbd218b27b6b47637.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cb104ad1a5b86584ca56995b0a9b706",
+    "url": "/react-online-store/index.html"
+  },
+  {
+    "revision": "e25b8b268035df2eef5e",
+    "url": "/react-online-store/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3835afb03ff864f064d2",
+    "url": "/react-online-store/static/js/2.2d0ae0b2.chunk.js"
+  },
+  {
+    "revision": "28d36cae8db7790f6fc02920470bed26",
+    "url": "/react-online-store/static/js/2.2d0ae0b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e25b8b268035df2eef5e",
+    "url": "/react-online-store/static/js/main.b12beae4.chunk.js"
+  },
+  {
+    "revision": "d13ae80509a71a60e195",
+    "url": "/react-online-store/static/js/runtime-main.f725dc44.js"
+  },
+  {
+    "revision": "8eb76e99d8d0c254657739730a5d818d",
+    "url": "/react-online-store/static/media/carrinho.8eb76e99.png"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/react-online-store/static/media/searchIcon.db812135.svg"
+  }
+]);
